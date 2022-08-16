d928db4eb4fec0443bd12c91ca61138cd4bac365
cromewar <vasiliy@cromewar.comr>
2022-08-16 11:07:12 -0500

    adding metadata for moralis into factory

--- contracts/DonatyNFTFactory.sol
+++ contracts/DonatyNFTFactory.sol
@@ -8,7 +8,19 @@ contract DonatyNFTFactory {
     //events
     event newNFTContractCreated(
         address indexed _nftArtist,
-        address indexed contractAddress
+        address indexed contractAddress,
+        uint256 _goal,
+        string indexed _initialDate,
+        string _dueDate,
+        uint256 _duration,
+        uint256 _steps,
+        string _ipfsImage
+    );
+
+    event newStepAdded(
+        string _description,
+        string _initialDate,
+        string _dueDate
     );
 
     // Array for contracts.
@@ -18,16 +30,46 @@ contract DonatyNFTFactory {
     // counter
     uint256 private contracts = 0;
 
-    function createNFTContract(string memory _nftName, string memory _tokenName)
-        public
-    {
+    function createNFTContract(
+        string memory _nftName,
+        string memory _tokenName,
+        uint256 _goal,
+        string[] memory _stepsInitialDate,
+        string[] memory _stepsDueDate,
+        string memory _initialDate,
+        string memory _dueDate,
+        uint256 _duration,
+        uint256 _stepDivision,
+        string memory _ipfsImage,
+        string[] memory _descriptionSteps
+    ) public {
         DonatyNFT newNFTContract = new DonatyNFT(_nftName, _tokenName);
         nftContracts.push(newNFTContract);
         contracts = contracts + 1;
-        emit newNFTContractCreated(msg.sender, address(newNFTContract));
+        emit newNFTContractCreated(
+            msg.sender,
+            address(newNFTContract),
+            _goal,
+            _initialDate,
+            _dueDate,
+            _duration,
+            _stepDivision,
+            _ipfsImage
+        );
+        emitEventForStep(_descriptionSteps, _stepsInitialDate, _stepsDueDate);
     }
 
     function getNFTContract(uint256 _index) public view returns (address) {
         return address(nftContracts[_index]);
+    }
+
+    function emitEventForStep(
+        string[] memory _steps,
+        string[] memory _initialDate,
+        string[] memory _dueDate
+    ) internal {
+        for (uint256 i = 0; i < _steps.length; i++) {
+            emit newStepAdded(_steps[i], _initialDate[i], _dueDate[i]);
+        }
     }
 }
