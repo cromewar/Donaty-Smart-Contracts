9a6782e6523e0e3a65c1ea9ee52076f16f461c69
cromewar <vasiliy@cromewar.comr>
2022-08-18 20:51:33 -0500

    deploying all factories

--- scripts/deploy_governance_factory.py
+++ scripts/deploy_governance_factory.py
@@ -17,45 +17,6 @@ def deploy_governance_factory():
     # Deploy Governor
     donaty_governance_factory = DonatyGovernanceFactory.deploy({"from": account})
     print(f"Governance Factory Deploy to {donaty_governance_factory.address}")
-    token_factory = DonatyNFT.deploy("Donaty", "DON", {"from": account})
-    # Deploy Time Lock
-    time_lock_factory = TimeLockFactory.deploy({"from": account})
-    tx = time_lock_factory.createNewTimeLock(
-        2, ZERO_ADDRESS, ZERO_ADDRESS, {"from": account}
-    )
-    tx.wait(1)
-    time_lock_factory.giveAdminControl(0, {"from": account})
-    tx2 = donaty_governance_factory.createNewDao(
-        token_factory.address, time_lock_factory.address, {"from": account}
-    )
-    tx2.wait(1)
-    print(f"Dao Deploy to {donaty_governance_factory.getGovernor(0)}")
-
-    governor = Contract.from_abi(
-        "DonatyGovernor", donaty_governance_factory.getGovernor(0), DonatyGovernor.abi
-    )
-    time_lock = Contract.from_abi(
-        "TimeLock", time_lock_factory.getTimeLock(0), TimeLock.abi
-    )
-
-    # Get Time Lock Roles
-    time_lock_executer_role = time_lock.EXECUTOR_ROLE()
-    time_lock_proposer_role = time_lock.PROPOSER_ROLE()
-    timelock_canceller_role = time_lock.CANCELLER_ROLE()
-
-    # Grant timeLock Roles to governor contract
-    tx3 = time_lock.grantRole(
-        time_lock_executer_role, governor.address, {"from": account}
-    )
-    tx3.wait(1)
-    tx4 = time_lock.grantRole(
-        time_lock_proposer_role, governor.address, {"from": account}
-    )
-    tx4.wait(1)
-    tx5 = time_lock.grantRole(
-        timelock_canceller_role, governor.address, {"from": account}
-    )
-    tx5.wait(1)
 
 
 def main():
--- scripts/deploy_time_lock_factory.py
+++ scripts/deploy_time_lock_factory.py
@@ -9,11 +9,11 @@ def deploy_time_lock_factory():
     account = get_account()
     # Deploy Time Lock
     time_lock_factory = TimeLockFactory.deploy({"from": account})
-    tx = time_lock_factory.createNewTimeLock(
-        time_delay, ZERO_ADDRESS, ZERO_ADDRESS, {"from": account}
-    )
-    tx.wait(1)
-    print(f"TimeLock Factory Deploy to {time_lock_factory.getTimeLock(0)}")
+    # tx = time_lock_factory.createNewTimeLock(
+    #     time_delay, ZERO_ADDRESS, ZERO_ADDRESS, {"from": account}
+    # )
+    # tx.wait(1)
+    # print(f"TimeLock Factory Deploy to {time_lock_factory.getTimeLock(0)}")
 
 
 def main():
