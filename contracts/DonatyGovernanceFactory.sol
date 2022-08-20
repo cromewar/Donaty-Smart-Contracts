aebd2eda33b215a16addc8c8fef5b6074468ea15
cromewar <vasiliy@cromewar.comr>
2022-08-20 02:55:46 -0500

    adding proposal details to governance contract

--- contracts/DonatyGovernanceFactory.sol
+++ contracts/DonatyGovernanceFactory.sol
@@ -8,8 +8,11 @@ pragma solidity ^0.8.0;
 contract DonatyGovernanceFactory {
     DonatyGovernor[] public governors;
 
+    mapping(address => uint256) public governorIndex;
+
     //events
     event NewGovernorCreated(address indexed daoAddress, address nftContract);
+    event newProposalCreated(address nftAddress, uint256 proposalId);
 
     function createNewDao(
         IVotes _token,
@@ -27,10 +30,30 @@ contract DonatyGovernanceFactory {
             proposalThreshhold
         );
         governors.push(newGovernor);
+        governorIndex[address(newGovernor)] = governors.length - 1;
+
         emit NewGovernorCreated(address(newGovernor), nftContract);
     }
 
     function getGovernor(uint256 _index) public view returns (address) {
         return address(governors[_index]);
+    }
+
+    function proposeVotation(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description,
+        address nftAddress,
+        address daoAddress
+    ) public {
+        uint256 governor = governorIndex[daoAddress];
+        uint256 proposeId = governors[governor].propose(
+            targets,
+            values,
+            calldatas,
+            description
+        );
+        emit newProposalCreated(nftAddress, proposeId);
     }
 }
